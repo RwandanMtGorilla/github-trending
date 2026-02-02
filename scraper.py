d3f4f3a09ce2d06bfee6e74cf8c8ab952b41b75e
15848 <1584810150@qq.com>
2026-02-02 16:23:34 +0800

    chore: update gitignore git user.name feat: add more languages for github-trending

--- scraper.py
+++ scraper.py
@@ -121,7 +121,7 @@ def job():
 
     ''' Start the scrape job
     '''
-    languages = ['', 'java', 'python', 'javascript', 'go', 'c', 'c++', 'c#', 'html', 'css', 'unknown']
+    languages = ['', 'python', 'jupyter-notebook','typescript', 'javascript', 'rust',  'java','go', 'c', 'c++', 'c#', 'html','shell', 'css', 'unknown']
     for lang in languages:
         results = scrape_lang(lang)
         write_markdown(lang, results, archived_contents)
